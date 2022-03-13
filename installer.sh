5211c543b14fc71d0f03f70cbc8b3cd8160af75d
mephistolist <49227141+mephistolist@users.noreply.github.com>
2022-03-13 20:49:52 +0000

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+rootcheck(){
+  if [ "$EUID" -ne 0 ]
+    then echo "You are not root/sudo/doas. Come back when you are."
+    exit
+  fi
+}
+
+rootcheck
+
+if [ -d /usr/portdeb ]; then
+  echo -e "\nIt seems /usr/portdeb already exists."
+  echo -e "If you are re-installing remove this directory and try again.\n"
+  exit
+else      
+  mkdir /usr/portdeb
+fi
+
+if dpkg -s devscripts dpkg-dev fakeroot 2&>1 2>/dev/null; then
+  break 2>/dev/null
+else
+  apt install dpkg-dev devscripts fakeroot -y
+fi
+
+chown -Rv _apt:root /usr/portdeb
+chmod -Rv 700 /usr/portdeb
+cp portdeb /usr/bin
+chmod +x /usr/bin/portdeb
+echo -e "\nInstall complete.\n"
